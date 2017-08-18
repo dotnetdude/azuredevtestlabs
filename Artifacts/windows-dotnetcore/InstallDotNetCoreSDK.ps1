932d05ccdc3babfc2fc3be07d685cdcdd85b3bb0
dotnetdude <suresh4job@gmail.com>
2017-08-18 14:13:49 +0000

    added .net core sdk

---
+++ Artifacts/windows-dotnetcore/InstallDotNetCoreSDK.ps1
@@ -0,0 +1,15 @@
+$dotNetCoreSDKurl = "https://aka.ms/dotnet-sdk-2.0.0-win-gs-x64"
+
+$dotNetCoreSDKDestination =  $PSScriptRoot + "\dotnet-sdk-2.0.0-win-gs-x64.exe"
+
+Write-Host "Downloading DotNet Core SDK"
+
+Invoke-WebRequest -Uri $dotNetCoreSDKurl -OutFile $dotNetCoreSDKDestination
+Write-Output "Downloaded. Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+Write-Host "Completed downloading DotNet Core SDK"
+
+Write-Host "Installing DotNet Core SDK"
+Start-Process -FilePath $dotNetCoreSDKDestination -ArgumentList '/install /quiet /norestart' -PassThru | Wait-Process
+
+Write-Host "Completed installing DotNet Core SDK"
