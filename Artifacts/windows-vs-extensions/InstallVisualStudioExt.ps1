1b43206a159f6b07cdd0ab4cd2a6468b11e7d608
dotnetdude <suresh4job@gmail.com>
2017-08-18 21:20:11 +0100

    Update InstallVisualStudioExt.ps1

--- Artifacts/windows-vs-extensions/InstallVisualStudioExt.ps1
+++ Artifacts/windows-vs-extensions/InstallVisualStudioExt.ps1
@@ -1,11 +1,15 @@
 $vsExtnDestination =  $PSScriptRoot + "\vsextensions.json"
+$vsixinstallerlocation = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VSIXInstaller.exe"
+
 $links = Get-Content -Raw -Path $vsExtnDestination | ConvertFrom-Json
+
 Foreach ($link in $links.extensions)
 {
   $uri = $link.url
   $name = $link.name
 
   $destination = $PSScriptRoot + "\" + $uri.Substring($uri.LastIndexOf("/")+1)
+  $argument = "/quiet " + $destination
 
   Write-Host "Downloading  $name from $uri"
 
@@ -15,7 +19,7 @@ Foreach ($link in $links.extensions)
 
   Write-Host "Installing $name from $destination"
 
-  Start-Process -FilePath $destination -ArgumentList '/install /quiet /norestart' -PassThru | Wait-Process
+  Start-Process -FilePath $vsixinstallerlocation -ArgumentList $argument -PassThru | Wait-Process
 
   Write-Host "Installed $name"
 
